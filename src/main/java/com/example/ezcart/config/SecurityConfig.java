805f1f51decc36210803f24ab3e13a86b522de03
vsomasvr <samskritamrutam@gmail.com>
2025-06-17 11:18:21 -0500

    implement oauth sec

--- src/main/java/com/example/ezcart/config/SecurityConfig.java
+++ src/main/java/com/example/ezcart/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.ezcart.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -13,6 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -22,11 +26,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity // Enables Spring Security's web security support and provides the Spring Security integration with the Spring Web MVC.
 public class SecurityConfig {
 
-    @Value("${okta.oauth2.issuer}")
-    private String issuer;
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
-    @Value("${okta.oauth2.client-id}")
-    private String clientId;
+    @Value("${okta.post-logout-redirect-uri}")
+    private String postLogoutRedirectUri;
 
     /**
      * Configures the security filter chain.
@@ -78,6 +82,7 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/", true) // Always redirect to the root after login
                 )
                 .logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
                         // oauth2Login() is stateful and uses an HttpSession to store the OAuth2AuthenticationToken.
                         // These steps are essential to invalidate the local session and clear security context.
                         .invalidateHttpSession(true)
@@ -86,5 +91,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri(postLogoutRedirectUri);
+        return successHandler;
     }
 }
